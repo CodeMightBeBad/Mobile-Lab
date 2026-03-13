8c322c5b2b0946c45eb465e0fdb7449a1422c5d5
matteo.crepaldi04 <matteo.crepaldi04@gmail.com>
2026-03-13 12:28:15 +0100

    Implemented travel details screen

--- 99-TravelDiary/app/src/main/java/com/example/traveldiary/ui/screens/TravelDetailsScreen.kt
+++ 99-TravelDiary/app/src/main/java/com/example/traveldiary/ui/screens/TravelDetailsScreen.kt
@@ -1,6 +1,120 @@
 package com.example.traveldiary.ui.screens
 
+import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Image
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material.icons.outlined.Share
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 class TravelDetailsScreen : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        setContent {
+            Scaffold(
+                topBar = { TravelDetailsTopBar() },
+                floatingActionButton = {
+                    FloatingActionButton(onClick = { }) {
+                        Icon(Icons.Outlined.Share, "Share")
+                    }
+                }
+            ) { innerPadding ->
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                        .padding(innerPadding)
+                        .fillMaxWidth()
+                ) {
+                    TravelImage()
+                    Spacer(Modifier.size(20.dp))
+                    TravelDetails()
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TravelDetailsTopBar() {
+    CenterAlignedTopAppBar(
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            titleContentColor = MaterialTheme.colorScheme.primary
+        ),
+        title = { Text("Travel Details") },
+        actions = {
+            IconButton(onClick = { }) {
+                Icon(Icons.Outlined.Settings, "Settings")
+            }
+        }
+    )
+}
+
+@Composable
+fun TravelImage() {
+    Spacer(Modifier.size(20.dp))
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .size(150.dp)
+            .clip(CircleShape)
+            .background(MaterialTheme.colorScheme.primary)
+            .padding(15.dp)
+    ) {
+        Icon(
+            imageVector = Icons.Outlined.Image,
+            contentDescription = "Travel Image",
+            modifier = Modifier
+                .fillMaxSize(),
+            tint = Color.White
+        )
+    }
+}
+
+@Composable
+fun TravelDetails() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Destination",
+            style = MaterialTheme.typography.headlineSmall
+        )
+
+        Text("01/01/2024")
+
+        Spacer(Modifier.size(30.dp))
+
+        Text("Description")
+    }
 }
