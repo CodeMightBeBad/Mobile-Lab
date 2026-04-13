29e6682317e8640680e4c29503d5395d786f14cc
Matt <matteo.crepaldi04@gmail.com>
2026-04-13 11:15:11 +0200

    Implemented camera interactions

---
+++ 07-CameraBase/app/src/main/java/com/example/camera/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.example.camera
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.example.camera.ui.theme.CameraTheme
+import com.example.camera.utils.rememberCameraLauncher
+import com.example.camera.utils.saveImageToStorage
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CameraTheme {
+                CameraScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun CameraScreen() {
+    val ctx = LocalContext.current
+    val (pictureUri, takePicture) = rememberCameraLauncher(
+        onPictureTaken = { uri -> saveImageToStorage(uri, ctx.contentResolver) }
+    )
+
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier.padding(innerPadding).fillMaxSize()
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+            ) {
+                Button(onClick = takePicture) {
+                    Text("Take a Picture")
+                }
+            }
+
+            pictureUri?.let {
+                AsyncImage(it, "Captured image")
+            }
+        }
+    }
+}
